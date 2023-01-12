bf2acfc78a9e14a9103efe0538496ab1971ae232
Agana-Joseph <josephagana8@gmail.com>
2023-01-11 16:45:59 -0800

    Data Structure

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-
-
 def search_replace(my_list, search, replace):
-    new_list = my_list[:]
-    for i in new_list:
-        if i == search:
-            new_list[new_list.index(i)] = replace
-    return 
+    return [replace if search == n else n for n in my_list]
+
